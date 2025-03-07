847d26614b19fc2c2fbc4f1c43bc2153528da7cf
chadhindsight <chadrickJ8@gmail.com>
2025-03-07 13:58:42 -0500

    Continue setup of initialState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { useState, createContext } from 'react'
 import './App.css'
 
+// Initial state to be used in App
+const initialState = {
+  products: [],
+  filteredProducts: [],
+  cartItems: [],
+  searchTerm: "",
+  selectedCategories: [],
+};
+
+
+function appReducer(state, action) {
+  // Reducer functions that will be used to update state when needed
+
+}
+
+// Create the Context 
 export const AppContext = createContext();
 
 function App({ children}) {
+  // initialize state
+  const [state, dispatch] = useReducer(appReducer, initialState);
 
   return (
     <AppContext.Provider value={{  }}>
