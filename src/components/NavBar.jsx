ef6d9a5c2a445635e4416c8237501003ab0b31e1
chadhindsight <chadrickJ8@gmail.com>
2025-03-08 14:40:11 -0500

    Ensure NavBar has access to the items in state

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,30 @@
-import { SearchBar } from "./SearchBar";
+import { AppBar, Toolbar, Badge, Box, IconButton } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { SearchBar } from './SearchBar';
+import { useAppContext } from '../context/globalState';
 
-// Remember that clicking on the shopping button should navigate users to the cart(perhaps routing?)
+export function NavBar() {
+    const { state } = useAppContext();
+
+
+  
+
+  return (
+    <AppBar position="static" color="default" sx={{ bgcolor: 'grey.900' }}>
+      <Toolbar>
+        {/* Search Bar component */}
+        <SearchBar />
+        
+        {/* Flexible spacer that pushes the cart icon to the right */}
+        <Box sx={{ flexGrow: 1 }} />
+        
+        <IconButton color="inherit">
+            {/* will display the number of items in the cart */}
+          <Badge badgeContent={state.cartItems.length} color="error">
+            <ShoppingCartIcon />
+          </Badge>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
