eba107f4bd23f367958e1c0b916e8fe00729c9be
chadhindsight <chadrickJ8@gmail.com>
2025-03-08 11:52:30 -0500

    Replace tailwind with MUI since it is more streamlined to setup

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,42 +1,55 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { InputBase, InputAdornment } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
 import { useAppContext } from '../context/globalState';
 
+// Styled component for the search input
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: '9999px', // rounded-full
+  backgroundColor: alpha(theme.palette.grey[700], 1), // bg-gray-700
+  width: '100%',
+  transition: theme.transitions.create('width'),
+  '&:focus-within': {
+    width: '100%', // focus:w-full
+  },
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 4), // p-2 pl-10
+    borderRadius: '9999px', // rounded-full
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+}));
+
 export function SearchBar() {
-    const { state, dispatch } = useAppContext();
+  const { state, dispatch } = useAppContext();
 
-    const onInputChange = (e) => {
-      // listens for changes and dispatches an action to update searchTerm in the global state
-      dispatch({ type: "SET_SEARCH_TERM", payload: e.target.value });
-    };
+  const onInputChange = (e) => {
+    // listens for changes and dispatches an action to update searchTerm in the global state
+    dispatch({ type: "SET_SEARCH_TERM", payload: e.target.value });
+  };
 
-    console.log(state.searchTerm);
-    return (
-        <div className="relative w-full">
-        <input
-          type="text"
-          id="search"
-          className="bg-gray-700 rounded-full p-2 pl-10 transition focus:w-full"
-          placeholder="Search products..."
-          value={state.searchTerm}
-          onChange={onInputChange}
-        />
-        <svg
-          className="absolute left-2 top-1/2 -translate-y-1/2"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-          <path d="M21 21l-6 -6" />
-        </svg>
-      </div>
-    );
-  }
+  console.log(state.searchTerm);
   
+  return (
+    <Search>
+      <StyledInputBase
+        id="search"
+        placeholder="Search products..."
+        value={state.searchTerm}
+        onChange={onInputChange}
+        startAdornment={
+          <InputAdornment position="start" sx={{ pl: 1 }}>
+            <SearchIcon />
+          </InputAdornment>
+        }
+      />
+    </Search>
+  );
+}
