7f030b1cdfbc8776647a128210db5a6ffdf5c528
chadhindsight <chadrickJ8@gmail.com>
2025-03-12 12:48:52 -0400

    TODO: decide what should the payload be

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -4,8 +4,11 @@ import { useAppContext } from '../context/globalState';
 export function SidePanel(){
     const { state, dispatch } = useAppContext();
 
+    const onCategoryClick = () =>{
+        // dispatch: filtering the list being displayed by what option the user clicks
+        dispatch({type: "TOGGLE_CATEGORY", payload}) // TODO: decide what should the payload be
+    }
 
-    // dispatch: filtering the list being displayed by what option the user clicks
     return (
         <>
         
