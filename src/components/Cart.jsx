dad901978bf151677193c679e10df112a9196946
chadhindsight <chadrickJ8@gmail.com>
2025-03-14 11:43:35 -0400

    Initial setup of cart component

---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import { useAppContext } from "../../context/globalState";
+
+export const Cart = () =>{
+    const {dispatch, state} = useAppContext();
+
+    return(
+        <>
+            
+        </>
+    )
+}
