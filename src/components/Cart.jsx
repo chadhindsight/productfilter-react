387b1a3f93e093bfbef440be838c024689e36849
chadhindsight <chadrickJ8@gmail.com>
2025-03-17 19:58:58 -0400

    Add Updated logic for adding/removing items from cart to account for quantities

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,17 @@
+import { Paper } from "@mui/material";
 import { useAppContext } from "../../context/globalState";
 
 export const Cart = () =>{
     const {dispatch, state} = useAppContext();
 
     return(
-        <>
-            
-        </>
+        <Paper elevation={3} sx={{ p: 2 }}>
+            <Typography variant="h6" gutterBottom>
+              Your Cart
+            </Typography>
+            {
+                
+            }
+        </Paper>
     )
 }
