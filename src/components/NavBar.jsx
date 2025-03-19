067a9a511a5da3fa8c91d99c0edf3b514d4d3de0
chadhindsight <chadrickJ8@gmail.com>
2025-03-19 15:05:29 -0400

    Navigate to the  cart page when cart icon gets clicked

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { AppBar, Toolbar, Badge, Box, IconButton } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { SearchBar } from './SearchBar';
 import { useAppContext } from '../context/globalState';
+import { Link } from 'react-router-dom';
 
 export function NavBar() {
     const { state } = useAppContext();
@@ -13,11 +14,10 @@ export function NavBar() {
     <AppBar position="static" color="default" sx={{ bgcolor: 'grey.900' }}>
       <Toolbar>
         <SearchBar />
-        
         {/* Flexible spacer that pushes the cart icon to the right */}
         <Box sx={{ flexGrow: 1 }} />
         
-        <IconButton color="inherit">
+        <IconButton color="inherit" component={Link} to="/cart">
             {/* will display the number of items in the cart */}
           <Badge badgeContent={state.cartItems.length} color="error">
             <ShoppingCartIcon />
