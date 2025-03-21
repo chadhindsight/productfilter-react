7a0fc738eb87eab08a3d25adaca180cd47df6dea
chadhindsight <chadrickJ8@gmail.com>
2025-03-21 16:42:40 -0400

    Add navbar tests

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,16 +14,16 @@ export function NavBar() {
   return (
     <AppBar position="static" color="default" sx={{ bgcolor: 'grey.900' }}>
       <Toolbar>
-      <IconButton aria-label="back" component={Link} to="/">
+      <IconButton aria-label="back" component={Link} to="/" >
             <ArrowBack  />
         </IconButton>
         <SearchBar />
         {/* Flexible spacer that pushes the cart icon to the right */}
         <Box sx={{ flexGrow: 1 }} />
         
-        <IconButton color="inherit" component={Link} to="/cart">
+        <IconButton color="inherit" component={Link} to="/cart" data-testid="cart-icon" >
             {/* will display the number of items in the cart */}
-          <Badge badgeContent={state.cartItems.length} color="error">
+          <Badge badgeContent={state.cartItems.length} color="error" data-testid="cart-badge">
             <ShoppingCartIcon />
           </Badge>
         </IconButton>
