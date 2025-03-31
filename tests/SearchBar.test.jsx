c67536deee81934b4e4d089ffb856f0867b60ffb
chadhindsight <chadrickJ8@gmail.com>
2025-03-31 10:40:13 -0400

    Add a test to ensure search bar is in document

--- tests/SearchBar.test.jsx
+++ tests/SearchBar.test.jsx
@@ -1,7 +1,16 @@
 import { describe, expect, it } from "vitest";
 import { SearchBar } from "../src/components/SearchBar";
+import { render, screen } from "@testing-library/react";
+import { AppProvider } from "../src/context/globalState";
 
 describe("Search Bar Component", ()=>{
-
+    it("renders the search bar", ()=>{
+        render(
+        <AppProvider>   
+            <SearchBar />
+        </AppProvider>
+    )
+    expect(screen.getByTestId('searchTing')).toBeInTheDocument();
+    })
 })
 
