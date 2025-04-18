c6f4444c470aeda47a9cb485411bdffa0913a895
chadhindsight <chadrickJ8@gmail.com>
2025-04-18 15:12:12 -0400

    Create Spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,5 +1,15 @@
 import { CircularProgress, Box } from "@mui/material";
 
 const LoadingSpinner = () =>{
-    
+    return (
+        <Box
+        display="flex" 
+        justifyContent="center" 
+        alignItems="center" 
+        minHeight="200px"
+      >
+            <CircularProgress />
+        </Box>
+    )
 }
+export default LoadingSpinner
